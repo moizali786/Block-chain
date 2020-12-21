aaaed9b5ad7cba32ccc0e099a790aa797cbb0329
SYED MOIZ ALI <75645283+moizali786@users.noreply.github.com>
2020-12-22 04:03:49 +0500

    Delete contact.js

--- pages/contact.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/contact', function(req, res){
-   res.send('This is Contact page.');
-});
-
-module.exports = router;
